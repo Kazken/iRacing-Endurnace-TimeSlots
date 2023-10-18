9768983b97d052d11a0c158ff4b89776a88b2fad
Kazken <49033805+Kazken@users.noreply.github.com>
2023-10-17 22:56:52 -0700

    Global Endurance Tour Timeslots

---
+++ GET Times.py
@@ -0,0 +1,47 @@
+import argparse
+
+def calculate_next_race_values(previous_z):
+    y = previous_z + 39600
+    z = y + 72000
+    k = z + 1098000
+    return y, z, k
+
+def generate_race_splits(races, initial_x):
+    previous_z = initial_x
+    for idx, race in enumerate(races, start=1):
+        y_next, z_next, k_next = calculate_next_race_values(previous_z)
+
+        # Calculate splits based on the requirements
+        if idx == 1:
+            split1 = initial_x
+            split2 = split1 + 39600
+            split3 = split2 + 72000
+        else:
+            # For subsequent races, ensure Split 1 is 489600 more than Split 3 of the previous race
+            split3_prev = split3
+            split1 = split3_prev + 1098000
+            split2 = split1 + 39600
+            split3 = split2 + 72000
+
+        print(f"\nRace at {race}")
+        print(f"Split 1 is in <t:{split1}:R> on <t:{split1}:F>")
+        print(f"Split 2 is in <t:{split2}:R> on <t:{split2}:F>")
+        print(f"Split 3 is in <t:{split3}:R> on <t:{split3}:F>")
+
+        # Update previous_z for the next iteration
+        previous_z = z_next
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate race splits based on input variables.")
+    parser.add_argument("initial_x", type=int, help="Initial value for x")
+    parser.add_argument("races", nargs="+", help="List of races")
+
+    args = parser.parse_args()
+
+    initial_x = args.initial_x
+    races = args.races
+
+    print("Results:")
+
+    # Generate race splits for the specified races
+    generate_race_splits(races, initial_x)
